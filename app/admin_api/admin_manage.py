b57cef5072efadbf349bf587057ac5226bb05a19
stonehe <didahuang@gmail.com>
2018-05-07 22:57:59 +0800

    install webargs and marshmallow

--- app/admin_api/admin_manage.py
+++ app/admin_api/admin_manage.py
@@ -1,13 +1,25 @@
 from flask_restful import Resource, abort
-
+from webargs import fields
+from webargs.flaskparser import use_args
 from ..models import Admin
 from . import admin_api_bp, admin_api
 
+hello_args = {
+    'name': fields.Str(required = True),
+    'age': fields.Str(required = True)
+}
+
+school_args = {
+    'name': fields.Str(required = True)
+}
+
 
 class SchoolList(Resource):
-    def get(self):
-        pass
+    @use_args(hello_args)
+    def get(self, args):
+        return 'Hello' + args['name'] + args['ages']
 
+    @use_args(school_args)
     def post(self):
         pass
 

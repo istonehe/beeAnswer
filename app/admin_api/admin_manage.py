7d1d2e36c7039be459fe570784172742eb3459c8
stonehe <didahuang@gmail.com>
2018-05-10 01:19:02 +0800

    admin is good

--- app/admin_api/admin_manage.py
+++ app/admin_api/admin_manage.py
@@ -1,3 +1,4 @@
+from flask import url_for
 from flask_restful import Resource, abort, marshal_with, fields as rfields
 from webargs import fields, ValidationError
 from webargs.flaskparser import use_args
@@ -6,40 +7,80 @@ from ..models import Admin, School
 from .. import db
 from . import admin_api_bp, admin_api
 
-hello_args = {
-    'name': fields.Str(required = True),
-    'age': fields.Str(required = True)
+schoollist_get_args = {
+    'page': fields.Int(),
+    'per_page': fields.Int()
 }
 
-school_args = {
+school_post_args = {
     'name': fields.Str(required = True, validate=lambda p: len(p) >= 3),
     'intro': fields.Str(required = False, default=''),
     'admin_email': fields.Email(required = True)
 }
 
-schoollist_outputs={
 
+school_list={
+    'schools':rfields.Nested({
+        'id': rfields.Integer,
+        'name': rfields.String,
+        'intro': rfields.String,
+        'admin': rfields.String,
+        'timestamp': rfields.DateTime(dt_format='rfc822'),
+        'url': rfields.Url(absolute=True, endpoint='admin_api.school')
+    }),
+    'prev':rfields.String,
+    'next':rfields.String,
+    'count': rfields.Integer
 }
 
-school_outputs = {
+school_created = {
+    'id': rfields.Integer,
     'name': rfields.String,
     'intro': rfields.String,
     'admin': rfields.String,
-    'url': rfields.Url(absolute=True)
+    'timestamp': rfields.DateTime(dt_format='rfc822'),
+    'url': rfields.Url(absolute=True, endpoint='admin_api.schools')
 }
 
 class SchoolList(Resource):
-    @use_args(hello_args)
+    @marshal_with(school_list, envelope='resource')
+    @use_args(schoollist_get_args)
     def get(self, args):
-        return 'Hello' + args['name'] + args['age']
+        pagination = School.query.paginate(
+            page=args['page'], per_page=args['per_page'], error_out=True
+        )
+        schools = pagination.items
+        prev = None
+        if pagination.has_prev:
+            prev = url_for('admin_api.school', id=args['page']-1, _external=True)
+        next = None
+        if pagination.has_next:
+            next = url_for('admin_api.school', id=args['page']+1, _external=True)
+        result = {
+        'schools':schools,
+        'prev': prev,
+        'next': next,
+        'count': pagination.total
+        }
+        return result,200
 
-    @marshal_with(school_outputs, envelope='resource')
-    @use_args(school_args)
+
+    @marshal_with(school_created, envelope='resource')
+    @use_args(school_post_args)
     def post(self, args):
         school = School(name=args['name'], intro=args['intro'], admin=args['admin_email'])
         db.session.add(school)
         db.session.commit()
         result = School.query.get(school.id)
-        return result,201
+        return result, 201
 
-admin_api.add_resource(SchoolList, '/school')
+class Schoolx(Resource):
+    @marshal_with(school_created, envelope='resource')
+    def get(self, id):
+        school = School.query.get(id)
+        return school, 200
+
+
+
+admin_api.add_resource(SchoolList, '/school', endpoint='schools')
+admin_api.add_resource(Schoolx, '/school/<id>', endpoint='school')
--- app/errors.py
+++ app/errors.py
@@ -2,7 +2,7 @@ from flask_restful import abort
 from sqlalchemy.exc import IntegrityError
 from .admin_api import admin_api_bp
 
-class ValidationError(ValueError, IntegrityError):
+class ValidationError(ValueError):
     pass
 
 def bad_request(message):
@@ -10,5 +10,9 @@ def bad_request(message):
 
 
 @admin_api_bp.errorhandler(IntegrityError)
+def validation_error(e):
+    return bad_request(e.args[0])
+#TypeError
+@admin_api_bp.errorhandler(ValueError)
 def validation_error(e):
     return bad_request(e.args[0])

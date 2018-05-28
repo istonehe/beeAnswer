3c4622e527195dc363ffa5fdbdacd9ce2fdba9f0
Noah <didahuang@gmail.com>
2018-05-28 17:44:33 +0800

    fabric testing

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JSON_AS_ASCII = False
     RESTFUL_JSON = dict(ensure_ascii=False)
-    UPLOAD_FOLDER = os.path.join(basedir, 'uploads', os.path.pardir)
+    UPLOAD_FOLDER = os.path.join(basedir, os.path.pardir, 'uploads')
     ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif', 'mp3'])
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024
 
